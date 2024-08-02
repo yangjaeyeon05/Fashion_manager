b3f4938a60bf79ffabeb1c2d82d0caf75103ab56
Kimviruscool <sinsa9122@naver.com>
2024-08-02 17:45:33 +0900

    24-08-02 주문관리 페이지 MAPPING HTML 생성완료

--- src/main/java/web/controller/ViewController.java
+++ src/main/java/web/controller/ViewController.java
@@ -12,4 +12,22 @@ public class ViewController {
         return "/index.html";   // templates 폴더내 반환할 경로와 파일명
     }
 
+
+    // 주문 목록 페이지
+    @GetMapping("/order")
+    public String order(){
+        return "/order.html";
+    }
+    
+    // 주문 반품 페이지
+    @GetMapping("order/return")
+    public String orderReturn(){
+        return "/orderReturn.html";
+    }
+
+    // 주문 취소 페이지
+    @GetMapping("/order/cancel")
+    public String orderCancel(){
+        return "/orderCancel";
+    }
 }   // class end
