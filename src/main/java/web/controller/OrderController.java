330a857aa50c72f142004d4ecd4ae740244a0065
Kimviruscool <sinsa9122@naver.com>
2024-08-05 17:54:21 +0900

    24-08-05 order

---
+++ src/main/java/web/controller/OrderController.java
@@ -0,0 +1,26 @@
+package web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.model.dto.OrderdetailDto;
+import web.sevice.OrderService;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController { //class start
+
+    @Autowired OrderService orderService;
+
+    //주문목록 출력 함수
+    @GetMapping("/getorder")
+    public ArrayList<OrderdetailDto> getorder(){
+        System.out.println("OrderController.getorder");
+        return orderService.getorder();
+    }
+
+}//class end
