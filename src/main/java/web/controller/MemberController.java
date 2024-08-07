ddb169e968867a919cfc4434154ba09e25592449
kimruafase <ppooppplus33@gmail.com>
2024-08-07 17:48:43 +0900

    2024-08-07 17:48 김민석 커밋

--- src/main/java/web/controller/MemberController.java
+++ src/main/java/web/controller/MemberController.java
@@ -7,6 +7,7 @@ import web.model.dto.MemberDto;
 import web.service.MemberService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/member")
@@ -23,9 +24,22 @@ public class MemberController {
 
     // 회원 정보 수정(블랙리스트만)
     @PutMapping("/edit")
-    public boolean memberEdit(int memcode, int blacklist){
-        System.out.println(memcode);
-        System.out.println(blacklist);
-        return memberService.memberEdit(MemberDto.builder().memcode(memcode).blacklist(blacklist).build());
+    public boolean memberEdit(@RequestBody MemberDto memberDto){
+
+        return memberService.memberEdit(memberDto);
     }
+
+    // -------------------------- 2024-08-05 ---------------------------------------- //
+
+
+    // 회원 사이즈에 맞는 옷 추천
+    @GetMapping("/recommend")
+    public List<Map<String, String>> memberRecommend(MemberDto memberDto){
+        System.out.println("memberDto = " + memberDto);
+        memberService.memberRecommend(memberDto);
+        return memberService.SizeRecommend(memberDto);
+    }
+
+    // -------------------------- 2024-08-07 ---------------------------------------- //
+
 }
--- src/main/java/web/service/MemberService.java
+++ src/main/java/web/service/MemberService.java
@@ -2,10 +2,15 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import web.model.dao.MemberDao;
 import web.model.dto.MemberDto;
+import web.model.dto.ProductDto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MemberService {
@@ -24,4 +29,40 @@ public class MemberService {
         System.out.println(memberDto.getBlacklist());
         return memberDao.memberEdit(memberDto);
     }
+
+    // -------------------------- 2024-08-05 ---------------------------------------- //
+
+    // 회원 번호에 맞는 사이즈 가져오기
+    public String memberRecommend(MemberDto memberDto){
+        String s = memberDao.memberRecommend(memberDto);
+        System.out.println(s);
+        return s;
+    }
+
+    // 가져온 회원의 선호 사이즈와 DAO 에서 받아온 정보를 반복문을 통해 사이즈가 일치하는 것만 새로운 LIST 에 저장함.
+    public List<Map<String, String>> SizeRecommend(MemberDto memberDto){
+        String s = memberRecommend(memberDto);
+        memberDto.setMemsize(s);
+        System.out.println(memberDto);
+        List<ProductDto> list = memberDao.SizeRecommend();
+        List<Map<String, String>> recommendList = new ArrayList<>();
+        Map<String, String> recommend = new HashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).getProdSize().equals(memberDto.getMemsize())){
+                recommend.put("size",list.get(i).getProdSize());
+                recommend.put("color",String.valueOf(list.get(i).getColorCode()));
+                recommend.put("name", list.get(i).getProdName());
+                recommend.put("price",String.valueOf(list.get(i).getProdPrice()));
+                recommend.put("desc",list.get(i).getProdDesc());
+                recommend.put("file",list.get(i).getProdFilename());
+                recommendList.add(recommend);
+            }
+        }
+        System.out.println(recommendList);
+        return recommendList;
+    }
+
+
+    // -------------------------- 2024-08-07 ---------------------------------------- //
+
 }
