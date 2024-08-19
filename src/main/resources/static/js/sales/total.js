4ff2a4e4258f4a833a6f2c759a147551bd511b70
syeon8344 <yeon8344@gmail.com>
2024-08-19 14:38:52 +0900

    0819 html css js

--- src/main/resources/static/js/sales/total.js
+++ src/main/resources/static/js/sales/total.js
@@ -1,68 +1,87 @@
-console.log('support.js');
 
-getSalesData()
+getTotalTable()
 
-// 테이블에 오늘부터 일주일 전까지 날짜 및 데이터 가져오는 함수
-function getSalesData(){
-    let today = new Date();  // 오늘 날짜
-    let date = new Date(today)  // 오늘 날짜 복사, 날짜 계산 용도
-    console.log(date);
-    table = ""  // 테이블 HTML
+// 연단위 매출 조회 (레코드 단위 : 연도)
+function getTotalTable(){
+    tableHTML = ""  // 테이블 HTML
     $.ajax({  // 테이블 데이터 가져오는 ajax
         async : false,
         method : "GET",
         url : "/sales/totaltable",
         success : r => {
-            console.log(r);
-            // 2000-00-00 방식으로 잘라낸 날짜 목록
-            let formatDates = []
-            // DTO 필터링할 날짜 목록
-            let days = []
-            // 필터링된 DTO 배열
-            let records = []
-            // 최근 일주일 날짜가 있는 테이블을 만들고 조회한 날짜 레코드가 있으면 채우고 없으면 0으로 채우기
-            // 테이블에 적을 날짜 목록 7칸
-            for(let i = 0; i < 7; i++){
-                date.setDate(today.getDate() - i);
-                let formatDate = date.toISOString().split("T")[0] // 2000-00-00
-                let formatDay = formatDate.split("-")[2]
-                days.push(parseInt(formatDay))
-                formatDates.push(formatDate)
-            }
-            console.log(days);
-            r.forEach(dto => {
-                console.log("dto day: " + dto.day);
-                // 입력할 레코드를 날짜를 이용해 필터링
-                if (days.includes(dto.day)){
-                    console.log("filtered");
-                    records.push(dto)
-                } 
-            })
-            console.log(records);
+            console.log("response");
+            let chartLabel = []
+            let revenueChartData = []
+            let saleAmountChartData = []
+            let incomeChartData = []
             // 테이블 HTML 생성 루프
-            for (let i = 0; i < 7; i++){
-                console.log("for문 루프중 " + formatDates[i]);
-                // 항상 날짜 행 7개 생성
-                table += `<tr><td>${formatDates[i]}</td>`
-                // 모든 칸을 0으로 채울지 체크
-                let hasData = false
-                // 필터링한 DTO 목록을 순회하며 날짜가 맞으면 데이터 입력
-                records.forEach(dto => {
-                    if (dto.day == days[i]){
-                        table += `<td>${dto.orders}</td><td>${dto.ordered}</td><td>${dto.returned}</td><td>${dto.canceled}</td><td>${dto.completed}</td><td>${dto.revenue}</td><td>${dto.saleAmount}</td><td>${dto.income}</td></tr>`
-                        hasData = true
-                    }
-                })
-                // forEach 루프 후 데이터가 안 들어있다 = 0으로 채울 칸
-                if (hasData == false){
-                    table += `<td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td></tr>`
-                }
+            r.forEach(dto => {                
+                chartLabel.push(dto.year)
+                revenueChartData.push(dto.revenue)
+                saleAmountChartData.push(dto.saleAmount)
+                incomeChartData.push(dto.income)
+                tableHTML += `<td>${dto.year}</td><td>${dto.orders}</td><td>${dto.ordered}</td><td>${dto.returned}</td><td>${dto.canceled}</td><td>${dto.completed}</td><td>${currencyFormat(dto.revenue)}</td><td>${currencyFormat(dto.saleAmount)}</td><td>${currencyFormat(dto.income)}</td></tr>`
+            })
+            console.log(chartLabel);
+            console.log(revenueChartData);
+            console.log(saleAmountChartData);
+            console.log(incomeChartData);
+
+            // 완성된 HTML을 삽입
+            document.querySelector("#tablePrintBox").innerHTML = tableHTML
+
+            if (!document.getElementById(revenueChart)){
+                createChart("revenueChart", chartLabel, revenueChartData,'총매출금액')
+            } else {
+                updateChartData(document.getElementById(revenueChart), newData)
+            }
+            if (!document.getElementById(saleAmountChart)){
+                createChart("saleAmountChart", chartLabel, saleAmountChartData,'할인금액')
+            } else {
+                updateChartData(document.getElementById(saleAmountChart), newData)
+            }
+            if (!document.getElementById(incomeChart)){
+                createChart("incomeChart", chartLabel, incomeChartData,'실주문금액')
+            } else {
+                updateChartData(document.getElementById(incomeChart), newData)
             }
         }
     })
-    // 완성된 HTML을 삽입
-    document.querySelector("#tableBox").innerHTML = table
 }
+
+// 차트 데이터 수정 및 업데이트
+function updateChartData(chart, newData) {
+    // 기존 데이터 수정
+    chart.data.datasets[0].data = newData; // 데이터셋의 데이터 업데이트
+
+    // 차트 업데이트
+    chart.update();
+}
+
+// 차트 생성
+function createChart(canvasId, lables, data, name){
+    
+    let myChart = new Chart(document.getElementById(canvasId), {
+        type: 'line',
+        data: {
+            labels: lables,
+            datasets: [{
+                label: name,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+        options: {
+            // chart options
+            responsive: true, // 화면 크기에 따라 차트 크기 조절
+        }
+    });
+}
+
+
 
 // 엑셀로 테이블 데이터 다운받기
 function excelExport(){
@@ -97,61 +116,9 @@ function excelExport(){
             xhr.send();
 }
 
-// 답변 내용 수정
-function createBox(replycode , supcode){
-    console.log('replyUpdate()');
-    // 수정을 위한 텍스트 입력 창 먼저 출력
-    // 어디에 
-    let replyUpdatebox = document.querySelector(".replyUpdatebox");
-    // 무엇을
-    let html = `
-                <th> 답변수정내용  </th>
-                <td colspan="5"> 
-                <div class="updateBox">
-                <input type="text" style="width:80%;"class="replycontent" placeholder="수정할 내용을 입력하세요" /> 
-                <button type="button" class="btn btn-success btn-sm" onclick="submitUpdate(${replycode} , ${supcode})">수정등록</button>
-                </div>
-                </td>
-                `;
-    // 출력
-    replyUpdatebox.innerHTML = html;
-    replyUpdate(replycode , supcode);
-}   // createBox() end
 
-// 답변 수정 요청을 서버로 보내는 함수
-function submitUpdate(replycode , supcode){
-    // 출력 후 입력된 값 가져오기
-    let replycontent = document.querySelector(".replycontent").value;
-    console.log(replycontent);
-    // 입력 내용이 비어 있는 경우 경고 메시지
-    if (!replycontent) {
-        alert('답변 내용을 입력해 주세요.');
-        return;
-    }
-    // 객체 만들어서 전달
-    let info = {
-        replycontent : replycontent , replycode : replycode
-    }
-    console.log(info);
-    // ajax 통신
-    $.ajax({
-        async : false , 
-        method : "put" , 
-        url : "/support/respedit" , 
-        data : JSON.stringify(info) , // 객체를 JSON 문자열로 변환
-        contentType : "application/json" , 
-        success : (r) => {
-            console.log(r);
-            if(r){
-                alert('답변수정성공');
-                replyRead(supcode);     // 새로고침
-                supDetailRead(supcode);
-            }else{
-                alert('답변수정실패')
-            }
-        } , 
-        error : (e) => {
-            console.log(e);
-        }
-    })  // ajax end
-}   // submitUpdate() end
+// 특정 열의 숫자를 원화 형식으로 변환
+function currencyFormat(value) {
+    return value.toLocaleString('ko-KR', { style: 'currency', currency: 'KRW' });
+}
+
--- src/main/resources/static/js/sales/yearly.js
+++ src/main/resources/static/js/sales/yearly.js
@@ -1,325 +1,135 @@
-console.log('support.js');
-let date = new Date();
-console.log(date);
-let currentYear = date.getFullYear();
-let currentMonth = date.getMonth()+1 < 10 ? "0"+(date.getMonth()+1) : date.getMonth()+1;
-let currentDay = date.getDate() < 10 ? "0"+(date.getDate()) : date.getDate();
-date = `${currentYear}-${currentMonth}-${currentDay}`;
-console.log(date)
+let revenueChart;
+let saleAmountChart;
+let incomeChart;
+let months = [1,2,3,4,5,6,7,8,9,10,11,12]
+getYearlyTable()
 
-// 날짜 검색 기능 8/7 검색 기능 구현 중
-document.querySelector('.endDate').value = date;
-// 날짜 버튼을 눌렀을 때
-function changeDate(day){
-    // startdate에 넣을 때 빼기로 설정을 해줘야하므로 설정에 따른 값 매개변수 day로 받아오기
-    console.log("changeDate()")
-    let startDateInput = document.querySelector(".startDate");
-    let startDate = new Date();     // 왼쪽 input
-    startDate.setDate(startDate.getDate()-day);     // 오늘 날짜에서 매개변수로 받아온 일수 만큼 빼주기 js라이브러리 함수
-    console.log(startDate);
-    let startYear = startDate.getFullYear();    console.log(startYear);
-    // 한자리수일 경우에 0 앞에 붙이는 삼항연산자
-    let startMonth = startDate.getMonth()+1 < 10 ? "0"+(startDate.getMonth()+1) : startDate.getMonth()+1;      
-    console.log(startMonth);
-    let startDay = startDate.getDate() < 10 ? "0"+(startDate.getDate()) : startDate.getDate();          
-    console.log(startDay);
-    let strStartDate = `${startYear}-${startMonth}-${startDay}`;    // input date 포맷이 문자열 "YY-MM-DD" 형식으로만 받기 떄문에 문자 따로 만들어주기
-    console.log(strStartDate)
-    startDateInput.value = strStartDate;
-    let endDateInput = document.querySelector(".endDate");  // 오른쪽 input
-    endDateInput.value = date;  // 컨셉 상 enddate는 오늘 날짜를 기준으로 함 전역변수로 설정해둔 오늘 날짜 대입.
-}
-
-// 검색 기능 객체 컨셉 상 검색 조건이 문의 유형별 , 처리상태별 , 검색별 , 기간별 이므로 해당하는 객체 만들어주기
-let searchInfo = {
-    supcategory : 0 , 
-    supstate : 0 , 
-    searchKey : '' , 
-    searchKeyword : '' , 
-    startDate : '' , 
-    endDate : '' , 
-}
-
-supAllread();
-// 검색버튼 눌렀을 때
-function onSearch(){
-    console.log('onSearch()');
-    let supcategory = document.querySelector('.supcodeBox').value;
-    let supstate = document.querySelector('.supstateBox').value;
-    let searchKey = document.querySelector('.searchKey').value;
-    let searchKeyword = document.querySelector('.searchKeyword').value;
-    let startDate = document.querySelector('.startDate').value;
-    let endDate = document.querySelector('.endDate').value;
-    searchInfo.supcategory = supcategory;
-    searchInfo.supstate = supstate;
-    searchInfo.searchKey = searchKey;
-    searchInfo.searchKeyword = searchKeyword;
-    searchInfo.startDate = startDate;
-    searchInfo.endDate = endDate;
-    console.log(searchInfo);
-    // 새로고침
-    supAllread();
-}
-// 상담목록 전체 출력
-function supAllread(){
-    console.log('supAllread()');
-    let supportList = [];
-    $.ajax({
-        async : false , 
-        method : 'get' , 
-        url : "/support/allread" ,
-        data : searchInfo , // 전역변수 보내기
-        success : (r) => {
-            console.log(r);
-            supportList = r;
-        } , 
-        error : (e) =>{
-            console.log(e); 
-        }
-    })  // ajax end
-    // 어디에
-    let supportPrintBox = document.querySelector(".supportPrintBox");
-    // 무엇을
-    let html = ``;
-    supportList.forEach( s => {
-            html += `<tr>
-                        <td> ${s.supcode} </td>
-                        <td> ${s.supdate} </td>
-                        <td> ${s.supstatename} </td>`;
-            if(s.ordcode != 0){
-                html += `<td> ${s.ordcode} </td>`;
-            }else{
-                html += `<td> </td>`;
-            }                    
-            html += `<td> ${s.supcategoryname} </td>
-                        <td>
-                            <a href="#" class="supDetailLink" data-bs-toggle="modal" data-bs-target="#staticBackdrop" data-id="${s.supcode}"> ${s.suptitle} </a> 
-                        </td>
-                        <td> ${s.memname} </td>
-                    </tr> `;
-        }
-    )   // forEach end
-    // 출력
-    supportPrintBox.innerHTML = html;
-    // 제목 클릭 시 모달 열기
-    document.querySelectorAll('.supDetailLink').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            let supcode = this.getAttribute('data-id');
-            supDetailRead(supcode);  // 모달에 상세 정보 로드
-        });
-    });
-}   // supAllread() end
-
-function supDetailRead(supcode){
-    console.log('supDetailRead()');
-    // 어디에
-    let supDtailPrintBox = document.querySelector('.supDtailPrintBox');
-    // 무엇을
-    let html = ``;
-    $.ajax({
-        async : false , 
-        method : "get" , 
-        url : "/support/read" , 
-        data : { "supcode" : supcode } , 
-        success : (r) => {
-            console.log(r);
-            html += `
-            <table class="table">
-                <tbody>
-                    <tr>
-                        <th> 접수일 </th>
-                        <td> ${r.supdate} </td>
-                        <th> 문의 유형 </th> 
-                        <td> ${r.supcategoryname} </td> 
-                        <th> 처리 상태 </th>
-                        <td> ${r.supstatename} </td>
-                    </tr>
-                    <tr>
-                        <th> 작성자명 </th>
-                        <td> ${r.memname} </td>
-                        <th> 작성자이메일</th> 
-                        <td> ${r.mememail} </td> 
-                        <th> 작성자연락처 </th>
-                        <td> ${r.memcontact} </td>
-                    </tr>
-                    <tr>
-                        <th> 주문번호 </th>
-                    `;
-                    if(r.ordcode != 0){             // 만약에 주문코드가 있으면 주문코드를 출력하고 
-                        html += `<td> ${r.ordcode} </td>`
-                    }else{                          // 없으면 공백
-                        html += `<td> </td>`;
+// 연단위 매출 조회 (레코드 단위 : 월)
+function getYearlyTable(){
+    tableHTML = ""  // 테이블 HTML
+    $.ajax({  // 테이블 데이터 가져오는 ajax
+        async : false,
+        method : "GET",
+        url : "/sales/yearlytable",
+        data : {year : document.querySelector(".year").value},
+        success : r => {
+            console.log("response");
+            let chartLabel = []
+            let revenueChartData = []
+            let saleAmountChartData = []
+            let incomeChartData = []
+            // 테이블 HTML 생성 루프
+            for(let i = 1; i <= months.length; i++){
+                let hasRecord = false
+                r.forEach(dto => {
+                    if(dto.month == i){
+                        hasRecord = true
+                        chartLabel.push(dto.month)
+                        revenueChartData.push(dto.revenue)
+                        saleAmountChartData.push(dto.saleAmount)
+                        incomeChartData.push(dto.income)
+                        tableHTML += `<td>${dto.month}</td><td>${dto.orders}</td><td>${dto.ordered}</td><td>${dto.returned}</td><td>${dto.canceled}</td><td>${dto.completed}</td><td>${currencyFormat(dto.revenue)}</td><td>${currencyFormat(dto.saleAmount)}</td><td>${currencyFormat(dto.income)}</td></tr>`
+                        return
                     }
-            html += `   <th> 상품코드</th> `;
-                    if(r.proddetailcode != 0){             // 만약에 상품코드가 있으면 상품코드를 출력하고 
-                        html += `<td> ${r.proddetailcode} </td>`
-                    }else{                          // 없으면 공백
-                        html += `<td> </td>`;
-                    }
-            html += `   <th> 상품이름</th> `;
-                    if(r.proddetailcode != 0){             // 만약에 상품코드가 있으면 상품코드를 출력하고 
-                        html += `<td> ${r.prodname} </td>`
-                    }else{                          // 없으면 공백
-                        html += `<td> </td>`;
-                    }
-            html += `</tr>
-                    <tr>
-                        <th> 문의내용 </th>
-                        <td colspan="5"> ${r.supcontent} </td>
-                    </tr>
-                    <tr class="replyPrintBox">
-                        <th> 답변내용  </th>
-                        <td colspan="5" class="replyPrint"> </td>
-                    </tr>
-                    <tr class="replyUpdatebox">
-                        
-                    </tr>
-                    </tbody>
-                    </table>
-                    <div class="replyBtn">
-                        <button type="button" class="btn btn-success btn-sm" onclick="replyAdd(${supcode})"> 답변등록 </button>
-                        <button type="button" class="btn btn-success btn-sm" onclick="replyDelete(${supcode})"> 답변삭제 </button>
-                        <button type="button" class="btn btn-success btn-sm" onclick="replyUpdateTocom(${supcode})" > 상담완료 </button>
-                    </div>    
-                        `;
-            if (supDtailPrintBox) {
-                supDtailPrintBox.innerHTML = html;
-            }
-            replyRead(supcode);
-            // 모달을 열기
-            // let modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
-            // modal.show();
-            // 부트스트랩 자체에서 실행되는데 한번 더 코드를 넣으니까 두개가 실행되서 하나 끄게 되면 하나 남아서 오류가 뜸
-        } , 
-        error : (e) => {
-            console.log(e);
-        }
-    })  // ajax end
-}
+                })
 
-// 답글 출력
-function replyRead(supcode){
-    console.log('replyRead()')
-    // 어디에
-    let replyPrint = document.querySelector('.replyPrint');
-    let html = ``;
-    $.ajax({
-        async : false , 
-        method : 'get' , 
-        url : "/support/respread" , 
-        data : {"supcode" : supcode } , 
-        success : (r) => {
-            console.log(r);
-            if (replyPrint) {               // document.querySelector로 요소 가져올 경우 선택한 요소가 
-                if(r.replycontent!=null){
-                    html += `<div class="replyBox">
-                            ${r.replycontent}
-                            <button type="button" class="btn btn-success btn-sm" onclick="createBox(${r.replycode} , ${supcode})">답변수정</button>
-                            </div>
-                            `;
-                }else{
-                    html += `<input style="width:100%;" type="text" class="replyContent" />`;
+                if (hasRecord == false) {
+                    chartLabel.push(i)
+                    revenueChartData.push(0)
+                    saleAmountChartData.push(0)
+                    incomeChartData.push(0)
+                    tableHTML += `<td>${i}</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>${currencyFormat(0)}</td><td>${currencyFormat(0)}</td><td>${currencyFormat(0)}</td></tr>`
                 }
-                replyPrint.innerHTML = html;   // null 일 수 있으므로 if 문으로 확인 출력 안그럼 오류가 뜸???
-            }        
-        } , 
-        error : (e) => {
-            console.log(e);
-        }
-    })  // ajax end
-}   // replyRead() end
-
-// 딥변 등록
-function replyAdd(supcode){
-    console.log('replyAdd()');
-    console.log(supcode);
-    let replycontent = document.querySelector(".replyContent").value;
-    let info = {
-        "replycontent" : replycontent , "supcode" : supcode
-    }
-    $.ajax({
-        async : false , 
-        method : 'post' , 
-        url : "/support/respadd" , 
-        data : JSON.stringify(info) , // 객체를 JSON 문자열로 변환
-        contentType : "application/json" , 
-        success : (r) => {
-            console.log(r);
-            if(r){
-                alert('답변등록성공');
-                replyUpdateToing(supcode);  // 처리상태 변경
-                replyRead(supcode);     // 새로고침
-
-            }else{
-                alert('답변등록실패')
             }
-        } , 
-        error : (e) => {
-            console.log(e);
+            // 완성된 HTML을 삽입
+            document.querySelector("#tablePrintBox").innerHTML = tableHTML
+            // 차트 생성
+            createRevenueChart(chartLabel, revenueChartData,'총매출금액')
+            createsaleAmountChart(chartLabel, saleAmountChartData,'할인금액')
+            createIncomeChart(chartLabel, incomeChartData,'실주문금액')
         }
-    })  // ajax end
-}   // replyAdd() end
+    })
 
-// 답변 등록 했을 때 처리상태 변경 상담 전 -> 진행 중
-function replyUpdateToing(supcode){
-    console.log('replyUpdateToing()');
-    $.ajax({
-        async : false , 
-        method : 'put' , 
-        url : "/support/edittoing" , 
-        data : {supcode : supcode} , 
-        success : (r) =>{
-            console.log(r)
-            replyRead(supcode);     // 답변 처리하면 답변 등록된 게 새로고침 되어야하고 전체출력이랑 상세 출력도 새로고침 되어야 한다.
-            supDetailRead(supcode);
-            supAllread();
-        } , 
-        error : (e) => {
-            console.log(e)
-        }
-    })  // ajax end
-}   // replyUpdateToing() end
+}
 
-// 처리 완료 눌렀을 때 처리상태 변경 진행 중 -> 처리 완료
-function replyUpdateTocom(supcode){
-    console.log('replyUpdateTocom()');
-    $.ajax({
-        async : false , 
-        method : 'put' , 
-        url : "/support/edittocom" , 
-        data : {supcode : supcode} , 
-        success : (r) =>{
-            console.log(r)
-            replyRead(supcode);     // 답변 처리하면 답변 등록된 게 새로고침 되어야하고 전체출력이랑 상세 출력도 새로고침 되어야 한다.
-            supDetailRead(supcode);
-            supAllread();
-        } , 
-        error : (e) => {
-            console.log(e)
-        }
-    })  // ajax end
-}   // replyUpdateTocom() end
+// 차트 생성
+function createRevenueChart(lables, data, name){
+    if (revenueChart){
+        revenueChart.destroy()
+    }
 
-// 답변삭제
-function replyDelete(supcode){
-    console.log('replyDelete()')
-    $.ajax({
-        async : false , 
-        method : 'delete' , 
-        url : "/support/respedelete" , 
-        data : {supcode : supcode} , 
-        success : (r) =>{
-            console.log(r)
-            replyRead(supcode);     // 답변 처리하면 답변 등록된 게 새로고침 되어야하고 전체출력이랑 상세 출력도 새로고침 되어야 한다.
-            supDetailRead(supcode);
-            supAllread();
-        } , 
-        error : (e) => {
-            console.log(e)
+    revenueChart = new Chart(document.getElementById('revenueChart'), {
+        type: 'line',
+        data: {
+            labels: lables,
+            datasets: [{
+                label: name,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+        options: {
+            // chart options
+            responsive: true, // 화면 크기에 따라 차트 크기 조절
         }
-    })  // ajax end
-}   // replyDelete() end
+    });
+}
+
+function createsaleAmountChart(lables, data, name){
+    if (saleAmountChart){
+        saleAmountChart.destroy()
+    }
+
+    saleAmountChart = new Chart(document.getElementById('saleAmountChart'), {
+        type: 'line',
+        data: {
+            labels: lables,
+            datasets: [{
+                label: name,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+        options: {
+            // chart options
+            responsive: true, // 화면 크기에 따라 차트 크기 조절
+        }
+    });
+}
+
+function createIncomeChart(lables, data, name){
+    if (incomeChart){
+        incomeChart.destroy()
+    }
+
+    incomeChart = new Chart(document.getElementById('incomeChart'), {
+        type: 'line',
+        data: {
+            labels: lables,
+            datasets: [{
+                label: name,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+        options: {
+            // chart options
+            responsive: true, // 화면 크기에 따라 차트 크기 조절
+        }
+    });
+}
+
+
+
 
 // 엑셀로 테이블 데이터 다운받기
 function excelExport(){
@@ -354,61 +164,9 @@ function excelExport(){
             xhr.send();
 }
 
-// 답변 내용 수정
-function createBox(replycode , supcode){
-    console.log('replyUpdate()');
-    // 수정을 위한 텍스트 입력 창 먼저 출력
-    // 어디에 
-    let replyUpdatebox = document.querySelector(".replyUpdatebox");
-    // 무엇을
-    let html = `
-                <th> 답변수정내용  </th>
-                <td colspan="5"> 
-                <div class="updateBox">
-                <input type="text" style="width:80%;"class="replycontent" placeholder="수정할 내용을 입력하세요" /> 
-                <button type="button" class="btn btn-success btn-sm" onclick="submitUpdate(${replycode} , ${supcode})">수정등록</button>
-                </div>
-                </td>
-                `;
-    // 출력
-    replyUpdatebox.innerHTML = html;
-    replyUpdate(replycode , supcode);
-}   // createBox() end
 
-// 답변 수정 요청을 서버로 보내는 함수
-function submitUpdate(replycode , supcode){
-    // 출력 후 입력된 값 가져오기
-    let replycontent = document.querySelector(".replycontent").value;
-    console.log(replycontent);
-    // 입력 내용이 비어 있는 경우 경고 메시지
-    if (!replycontent) {
-        alert('답변 내용을 입력해 주세요.');
-        return;
-    }
-    // 객체 만들어서 전달
-    let info = {
-        replycontent : replycontent , replycode : replycode
-    }
-    console.log(info);
-    // ajax 통신
-    $.ajax({
-        async : false , 
-        method : "put" , 
-        url : "/support/respedit" , 
-        data : JSON.stringify(info) , // 객체를 JSON 문자열로 변환
-        contentType : "application/json" , 
-        success : (r) => {
-            console.log(r);
-            if(r){
-                alert('답변수정성공');
-                replyRead(supcode);     // 새로고침
-                supDetailRead(supcode);
-            }else{
-                alert('답변수정실패')
-            }
-        } , 
-        error : (e) => {
-            console.log(e);
-        }
-    })  // ajax end
-}   // submitUpdate() end
+// 특정 열의 숫자를 원화 형식으로 변환
+function currencyFormat(value) {
+    return value.toLocaleString('ko-KR', { style: 'currency', currency: 'KRW' });
+}
+
